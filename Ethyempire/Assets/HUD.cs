93df5e5346afa8d19e0bcc13b56db8fce4e8ab7a
PapiCarryFabs <fabiandresbarreto@gmail.com>
2019-05-14 00:25:59 -0500

    Version 0.5

---
+++ Ethyempire/Assets/HUD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Sprite[] HeartSprites;
+    public Image HeartUI;
+    private PlayerController Player;
+
+    void Start(){
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+    void FixedUpdate(){
+        HeartUI.sprite = HeartSprites[Player.curHealth];
+    }
+}
